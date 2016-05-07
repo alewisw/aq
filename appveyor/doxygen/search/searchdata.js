c083ffa8445cca67c96c027cf1d4aa75a69641eb
AppVeyor <alewis.sw@gmail.com>
2016-05-07 03:06:24 +0000

    Updated doxygen comments by AppVeyor CI.

--- appveyor/doxygen/search/searchdata.js
+++ appveyor/doxygen/search/searchdata.js
@@ -1,18 +1,33 @@
 var indexSectionsWithContent =
 {
-  0: "a",
-  1: "a"
+  0: "acfilnopqrsvw~",
+  1: "a",
+  2: "acfilnopqrsvw~",
+  3: "oq",
+  4: "ci",
+  5: "cw",
+  6: "a"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "pages"
+  1: "classes",
+  2: "functions",
+  3: "variables",
+  4: "enums",
+  5: "enumvalues",
+  6: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Pages"
+  1: "Classes",
+  2: "Functions",
+  3: "Variables",
+  4: "Enumerations",
+  5: "Enumerator",
+  6: "Pages"
 };
 
